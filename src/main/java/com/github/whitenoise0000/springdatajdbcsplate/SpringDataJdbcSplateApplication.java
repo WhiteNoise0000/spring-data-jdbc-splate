2af943e361c56879d0868326bb1895c2f3031387
WhiteNoise0000 <46626065+WhiteNoise0000@users.noreply.github.com>
2023-03-05 01:17:32 +0900

    初期資材コミット

---
+++ src/main/java/com/github/whitenoise0000/springdatajdbcsplate/SpringDataJdbcSplateApplication.java
@@ -0,0 +1,13 @@
+package com.github.whitenoise0000.springdatajdbcsplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJdbcSplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJdbcSplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/whitenoise0000/springdatajdbcsplate/SpringDataJdbcSplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.whitenoise0000.springdatajdbcsplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJdbcSplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
