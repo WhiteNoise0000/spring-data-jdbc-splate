37ddc856b8ba35aad314607852b6f4bf0bef6a78
WhiteNoise0000 <white.noise.0000@gmail.com>
2025-08-14 09:31:19 +0900

    JdbcRepositoryFactoryBeanを直接継承するよう修正

--- src/main/java/io/github/whitenoise0000/springdatajdbcsplate/SplateRepositoryFactoryBean.java
+++ src/main/java/io/github/whitenoise0000/springdatajdbcsplate/SplateRepositoryFactoryBean.java
@@ -33,167 +33,192 @@ import org.springframework.data.relational.core.dialect.Dialect;
 import org.springframework.data.relational.core.mapping.RelationalMappingContext;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.core.support.RepositoryFactorySupport;
-import org.springframework.data.repository.core.support.TransactionalRepositoryFactoryBeanSupport;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.util.Assert;
 
-
 /**
- * Spring Data JDBC向けSplate(2Way-SQL)ラッパー用FactoryBean.<br/>
- * <a href="https://cero-t.hatenadiary.jp/entry/2022/12/26/051831">参考URL</a>でも言及あるが、
- * {@link JdbcRepositoryFactoryBean}内のメンバ変数がprivateで隠蔽されており、
- * サブクラスから参照不可のため苦肉の策として全コードをコピー流用している.
+ * Spring Data JDBC向けSplate(2Way-SQL)ラッパー用FactoryBean.
+ * 
+ * <p>このFactoryBeanは、Splateを使用したRepositoryの生成をサポートします。
+ * Splateは、2Way-SQLを使用したデータアクセスを提供し、より直観的で効率的なデータ操作を可能にします。</p>
  * 
  * @see org.springframework.data.jdbc.repository.support.JdbcRepositoryFactoryBean
  */
 public class SplateRepositoryFactoryBean<T extends Repository<S, ID>, S, ID extends Serializable>
-		extends TransactionalRepositoryFactoryBeanSupport<T, S, ID> {
+    extends JdbcRepositoryFactoryBean<T, S, ID> {
 
-	private ApplicationEventPublisher publisher;
-	private BeanFactory beanFactory;
-	private RelationalMappingContext mappingContext;
-	private JdbcConverter converter;
-	private DataAccessStrategy dataAccessStrategy;
-	private QueryMappingConfiguration queryMappingConfiguration = QueryMappingConfiguration.EMPTY;
-	private NamedParameterJdbcOperations operations;
-	private EntityCallbacks entityCallbacks;
-	private Dialect dialect;
+    private ApplicationEventPublisher publisher;
+    private BeanFactory beanFactory;
+    private RelationalMappingContext mappingContext;
+    private JdbcConverter converter;
+    private DataAccessStrategy dataAccessStrategy;
+    private QueryMappingConfiguration queryMappingConfiguration = QueryMappingConfiguration.EMPTY;
+    private NamedParameterJdbcOperations operations;
+    private EntityCallbacks entityCallbacks;
+    private Dialect dialect;
 
-	/**
-	 * Creates a new {@link SplateRepositoryFactoryBean} for the given repository interface.
-	 *
-	 * @param repositoryInterface must not be {@literal null}.
-	 */
-	public SplateRepositoryFactoryBean(Class<? extends T> repositoryInterface) {
-		super(repositoryInterface);
-	}
+    /**
+     * 指定されたRepositoryインターフェース用の {@link SplateRepositoryFactoryBean} を新規作成します。
+     * 
+     * @param repositoryInterface Repositoryインターフェースのクラス、nullにできません。
+     */
+    public SplateRepositoryFactoryBean(Class<? extends T> repositoryInterface) {
+        super(repositoryInterface);
+    }
 
-	@Override
-	public void setApplicationEventPublisher(ApplicationEventPublisher publisher) {
+    /**
+     * アプリケーションイベントパブリッシャーを設定します。
+     *
+     * @param publisher 設定するApplicationEventPublisher
+     */
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher publisher) {
+        super.setApplicationEventPublisher(publisher);
+        this.publisher = publisher;
+    }
 
-		super.setApplicationEventPublisher(publisher);
+    /**
+     * 実際の {@link RepositoryFactorySupport} インスタンスを生成します。
+     *
+     * @return 生成されたRepositoryFactorySupportインスタンス
+     */
+    @Override
+    protected RepositoryFactorySupport doCreateRepositoryFactory() {
 
-		this.publisher = publisher;
-	}
+        // SplateRepositoryFactoryを生成
+        SplateRepositoryFactory jdbcRepositoryFactory = new SplateRepositoryFactory(dataAccessStrategy, mappingContext,
+                converter, dialect, publisher, operations);
+        jdbcRepositoryFactory.setQueryMappingConfiguration(queryMappingConfiguration);
+        jdbcRepositoryFactory.setEntityCallbacks(entityCallbacks);
+        jdbcRepositoryFactory.setBeanFactory(beanFactory);
 
-	/**
-	 * Creates the actual {@link RepositoryFactorySupport} instance.
-	 */
-	@Override
-	protected RepositoryFactorySupport doCreateRepositoryFactory() {
+        return jdbcRepositoryFactory;
+    }
 
-		// TODO private→protected変更時はコピー流用取りやめ
-		// ★ここだけ書き換え
-		SplateRepositoryFactory jdbcRepositoryFactory = new SplateRepositoryFactory(dataAccessStrategy, mappingContext,
-				converter, dialect, publisher, operations);
-		jdbcRepositoryFactory.setQueryMappingConfiguration(queryMappingConfiguration);
-		jdbcRepositoryFactory.setEntityCallbacks(entityCallbacks);
-		jdbcRepositoryFactory.setBeanFactory(beanFactory);
+    /**
+     * リレーショナルマッピングコンテキストを設定します。
+     *
+     * @param mappingContext 設定するRelationalMappingContext
+     */
+    @Override
+    public void setMappingContext(RelationalMappingContext mappingContext) {
+        super.setMappingContext(mappingContext);
+        this.mappingContext = mappingContext;
+    }
 
-		return jdbcRepositoryFactory;
-	}
+    /**
+     * ダイアレクトを設定します。
+     *
+     * @param dialect 設定するDialect
+     */
+    @Override
+    public void setDialect(Dialect dialect) {
+        super.setDialect(dialect);
+        this.dialect = dialect;
+    }
 
-	public void setMappingContext(RelationalMappingContext mappingContext) {
+    /**
+     * データアクセスストラテジーを設定します。
+     *
+     * @param dataAccessStrategy 設定するDataAccessStrategy
+     */
+    @Override
+    public void setDataAccessStrategy(DataAccessStrategy dataAccessStrategy) {
+        super.setDataAccessStrategy(dataAccessStrategy);
+        this.dataAccessStrategy = dataAccessStrategy;
+    }
 
-		Assert.notNull(mappingContext, "MappingContext must not be null");
+    /**
+     * クエリマッピング設定を設定します。
+     *
+     * @param queryMappingConfiguration 設定するQueryMappingConfiguration
+     */
+    @Autowired(required = false)
+    @Override
+    public void setQueryMappingConfiguration(QueryMappingConfiguration queryMappingConfiguration) {
+        super.setQueryMappingConfiguration(queryMappingConfiguration);
+        this.queryMappingConfiguration = queryMappingConfiguration;
+    }
 
-		super.setMappingContext(mappingContext);
-		this.mappingContext = mappingContext;
-	}
+    /**
+     * JDBC操作を設定します。
+     *
+     * @param operations 設定するNamedParameterJdbcOperations
+     */
+    @Override
+    public void setJdbcOperations(NamedParameterJdbcOperations operations) {
+        super.setJdbcOperations(operations);
+        this.operations = operations;
+    }
 
-	public void setDialect(Dialect dialect) {
+    /**
+     * コンバーターを設定します。
+     *
+     * @param converter 設定するJdbcConverter
+     */
+    @Override
+    public void setConverter(JdbcConverter converter) {
+        super.setConverter(converter);
+        this.converter = converter;
+    }
 
-		Assert.notNull(dialect, "Dialect must not be null");
+    /**
+     * BeanFactoryを設定します。
+     *
+     * @param beanFactory 設定するBeanFactory
+     */
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) {
+        super.setBeanFactory(beanFactory);
+        this.beanFactory = beanFactory;
+    }
 
-		this.dialect = dialect;
-	}
+    /**
+     * プロパティが設定された後に呼び出されます。
+     * 必要なプロパティが設定されていることを検証し、不足している場合はデフォルト値を設定します。
+     */
+    @Override
+    public void afterPropertiesSet() {
 
-	/**
-	 * @param dataAccessStrategy can be {@literal null}.
-	 */
-	public void setDataAccessStrategy(DataAccessStrategy dataAccessStrategy) {
+        Assert.state(this.mappingContext != null, "MappingContext is required and must not be null");
+        Assert.state(this.converter != null, "RelationalConverter is required and must not be null");
 
-		Assert.notNull(dataAccessStrategy, "DataAccessStrategy must not be null");
+        if (this.operations == null) {
 
-		this.dataAccessStrategy = dataAccessStrategy;
-	}
+            Assert.state(beanFactory != null, "If no JdbcOperations are set a BeanFactory must be available");
 
-	/**
-	 * @param queryMappingConfiguration can be {@literal null}. {@link #afterPropertiesSet()} defaults to
-	 *          {@link QueryMappingConfiguration#EMPTY} if {@literal null}.
-	 */
-	@Autowired(required = false)
-	public void setQueryMappingConfiguration(QueryMappingConfiguration queryMappingConfiguration) {
+            this.operations = beanFactory.getBean(NamedParameterJdbcOperations.class);
+        }
 
-		Assert.notNull(queryMappingConfiguration, "QueryMappingConfiguration must not be null");
+        if (this.dataAccessStrategy == null) {
 
-		this.queryMappingConfiguration = queryMappingConfiguration;
-	}
+            Assert.state(beanFactory != null, "If no DataAccessStrategy is set a BeanFactory must be available");
 
-	public void setJdbcOperations(NamedParameterJdbcOperations operations) {
+            this.dataAccessStrategy = this.beanFactory.getBeanProvider(DataAccessStrategy.class) //
+                    .getIfAvailable(() -> {
 
-		Assert.notNull(operations, "NamedParameterJdbcOperations must not be null");
+                        Assert.state(this.dialect != null, "Dialect is required and must not be null");
 
-		this.operations = operations;
-	}
+                        SqlGeneratorSource sqlGeneratorSource = new SqlGeneratorSource(this.mappingContext, this.converter,
+                                this.dialect);
+                        SqlParametersFactory sqlParametersFactory = new SqlParametersFactory(this.mappingContext, this.converter);
+                        InsertStrategyFactory insertStrategyFactory = new InsertStrategyFactory(this.operations, this.dialect);
 
-	public void setConverter(JdbcConverter converter) {
+                        DataAccessStrategyFactory factory = new DataAccessStrategyFactory(sqlGeneratorSource, this.converter,
+                                this.operations, sqlParametersFactory, insertStrategyFactory);
 
-		Assert.notNull(converter, "JdbcConverter must not be null");
+                        return factory.create();
+                    });
+        }
 
-		this.converter = converter;
-	}
+        if (this.queryMappingConfiguration == null) {
+            this.queryMappingConfiguration = QueryMappingConfiguration.EMPTY;
+        }
 
-	@Override
-	public void setBeanFactory(BeanFactory beanFactory) {
+        if (beanFactory != null) {
+            entityCallbacks = EntityCallbacks.create(beanFactory);
+        }
 
-		super.setBeanFactory(beanFactory);
-
-		this.beanFactory = beanFactory;
-	}
-
-	@Override
-	public void afterPropertiesSet() {
-
-		Assert.state(this.mappingContext != null, "MappingContext is required and must not be null");
-		Assert.state(this.converter != null, "RelationalConverter is required and must not be null");
-
-		if (this.operations == null) {
-
-			Assert.state(beanFactory != null, "If no JdbcOperations are set a BeanFactory must be available");
-
-			this.operations = beanFactory.getBean(NamedParameterJdbcOperations.class);
-		}
-
-		if (this.dataAccessStrategy == null) {
-
-			Assert.state(beanFactory != null, "If no DataAccessStrategy is set a BeanFactory must be available");
-
-			this.dataAccessStrategy = this.beanFactory.getBeanProvider(DataAccessStrategy.class) //
-					.getIfAvailable(() -> {
-
-						Assert.state(this.dialect != null, "Dialect is required and must not be null");
-
-						SqlGeneratorSource sqlGeneratorSource = new SqlGeneratorSource(this.mappingContext, this.converter,
-								this.dialect);
-						SqlParametersFactory sqlParametersFactory = new SqlParametersFactory(this.mappingContext, this.converter);
-						InsertStrategyFactory insertStrategyFactory = new InsertStrategyFactory(this.operations, this.dialect);
-
-						DataAccessStrategyFactory factory = new DataAccessStrategyFactory(sqlGeneratorSource, this.converter,
-								this.operations, sqlParametersFactory, insertStrategyFactory);
-
-						return factory.create();
-					});
-		}
-
-		if (this.queryMappingConfiguration == null) {
-			this.queryMappingConfiguration = QueryMappingConfiguration.EMPTY;
-		}
-
-		if (beanFactory != null) {
-			entityCallbacks = EntityCallbacks.create(beanFactory);
-		}
-
-		super.afterPropertiesSet();
-	}
+        super.afterPropertiesSet();
+    }
 }
