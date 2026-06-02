7c1250801a35f6af74be4f75418ab38d7c3a8b8d
WhiteNoise0000 <white.noise.0000@gmail.com>
2026-06-02 22:32:43 +0900

    feat: @Splateメソッドの単一JavaBean引数をサポート
    - SqlateQuery#getContext で単一引数がJavaBean展開対象型の場合、
      splate 0.3 の BeanPropertySqlTemplateContext を使うように分岐を追加
    - null の単一JavaBean引数は IllegalArgumentException を投げる
      (メッセージに 'JavaBean' と 'null' を含む)
    - Number / String / Date / java.time / Map / Iterable / Optional / 配列 /
      primitive / ラッパーはJavaBean展開対象外 (従来通り引数名で扱う)
    - テスト用JavaBean (EmployeeSearchCondition / EmployeeCreateCommand) を追加
    - 単一Bean引数による SELECT / COUNT / INSERT と null例外のテストを追加
    - READMEに対応範囲と対象外を追記
    - CHANGELOG (Unreleased) に Added セクションを追加

--- src/main/java/io/github/whitenoise0000/springdatajdbcsplate/SqlateQuery.java
+++ src/main/java/io/github/whitenoise0000/springdatajdbcsplate/SqlateQuery.java
@@ -1,5 +1,12 @@
 package io.github.whitenoise0000.springdatajdbcsplate;
 
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.repository.query.Parameter;
 import org.springframework.data.repository.query.QueryMethod;
 import org.springframework.data.repository.query.RepositoryQuery;
 import org.springframework.data.util.Lazy;
@@ -7,6 +14,7 @@ import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.RowMapper;
 
+import com.github.mygreen.splate.BeanPropertySqlTemplateContext;
 import com.github.mygreen.splate.EmptyValueSqlTemplateContext;
 import com.github.mygreen.splate.MapSqlTemplateContext;
 import com.github.mygreen.splate.ProcessResult;
@@ -16,7 +24,7 @@ import com.github.mygreen.splate.SqlTemplateEngine;
 
 /**
  * Splate(2-waySQL)クエリ実行.
- * 
+ *
  * @author WhiteNoise0000
  */
 class SqlateQuery implements RepositoryQuery {
@@ -90,12 +98,83 @@ class SqlateQuery implements RepositoryQuery {
 			return new EmptyValueSqlTemplateContext();
 		}
 
-		// TODO Bean引数
-		// 引数あり
+		// 単一引数
+		Iterator<? extends Parameter> it = queryMethod.getParameters().iterator();
+		if (it.hasNext()) {
+			Parameter param = it.next();
+			if (!it.hasNext()) {
+				Class<?> type = param.getType();
+				// JavaBeanとして展開する型のみ、BeanPropertySqlTemplateContext を利用する
+				if (isJavaBeanArgument(type)) {
+					Object value = params[param.getIndex()];
+					if (value == null) {
+						throw new IllegalArgumentException(
+								"@Splate method '" + queryMethod.getName()
+										+ "' does not accept a null JavaBean argument."
+										+ " Provide a non-null JavaBean instance.");
+					}
+					return new BeanPropertySqlTemplateContext(value);
+				}
+			}
+		}
+
+		// 引数あり(複数 / 単一スカラー)
 		MapSqlTemplateContext context = new MapSqlTemplateContext();
 		queryMethod.getParameters().forEach(param -> {
 			context.addVariable(param.getName().get(), params[param.getIndex()]);
 		});
 		return context;
+	}
+
+	/**
+	 * 単一引数をJavaBeanとして展開する対象かどうかを判定する.
+	 *
+	 * <p>{@code false} を返す型は、Bean展開対象外として従来通り引数名で
+	 * {@link MapSqlTemplateContext} に登録される.</p>
+	 */
+	private static boolean isJavaBeanArgument(Class<?> type) {
+		if (type == null) {
+			return false;
+		}
+		if (type.isPrimitive()) {
+			return false;
+		}
+		if (type.isArray()) {
+			return false;
+		}
+		if (Map.class.isAssignableFrom(type)) {
+			return false;
+		}
+		if (Iterable.class.isAssignableFrom(type)) {
+			return false;
+		}
+		if (Optional.class.isAssignableFrom(type)) {
+			return false;
+		}
+		if (Number.class.isAssignableFrom(type)) {
+			return false;
+		}
+		if (Boolean.class.equals(type)) {
+			return false;
+		}
+		if (Character.class.equals(type)) {
+			return false;
+		}
+		if (Enum.class.isAssignableFrom(type)) {
+			return false;
+		}
+		if (String.class.equals(type)) {
+			return false;
+		}
+		if (Date.class.isAssignableFrom(type)) {
+			return false;
+		}
+		if (UUID.class.equals(type)) {
+			return false;
+		}
+		if (type.getPackageName().startsWith("java.time")) {
+			return false;
+		}
+		return true;
 	}
 }
